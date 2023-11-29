91a7888a3527693058f6bcd462eceeed4682859a
Jan Lemmens <>
2023-11-29 11:43:28 +0100

    no message

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -9,6 +9,7 @@ export default function decorate(block) {
   var initial_quote = parts[0];
   var blockquote = document.createElement("blockquote");
   blockquote.innerHTML = initial_quote.innerHTML;
+  blockquote.className = 'aem-testimonial__quote aem-testimonial__quote--large';
 
   block.prepend(blockquote);
   block.prepend(icon);
