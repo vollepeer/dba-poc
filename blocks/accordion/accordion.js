bd45db4164e057975629e09cd4f2a10119018a05
Jan Lemmens <>
2023-11-29 14:11:37 +0100

    no message

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,87 +1,62 @@
 import {
-  buildBlock,
-  decorateBlock,
-  loadBlock,
-} from '../../scripts/aem.js';
+  dl, dt, dd, div, h3, button, span,
+} from '../../scripts/dom-builder.js';
+import { generateUUID } from '../../scripts/scripts.js';
 
-const toggle = (item) => {
-  const trigger = item.querySelector('.accordion-trigger');
-  const panel = item.querySelector('.accordion-panel');
-  const isOpen = trigger.getAttribute('aria-expanded') === 'true';
-  trigger.setAttribute('aria-expanded', isOpen ? 'false' : 'true');
-  if (isOpen) {
-    panel.setAttribute('hidden', '');
-  } else {
-    panel.removeAttribute('hidden');
-  }
-};
+function toggleAccordion(activeButton) {
+  const isOpen = activeButton.classList.contains('show');
+  activeButton.setAttribute('aria-expanded', !isOpen);
+  activeButton.classList.toggle('show', !isOpen);
+  activeButton.querySelector('span svg').classList.toggle('rotate-180', !isOpen);
+}
 
-const buildSubAccordion = async (parentPanel, blockTable) => {
-  const block = buildBlock('accordion', blockTable);
-  block.classList.add('sub-accordion');
-  parentPanel.append(block);
-  decorateBlock(block);
-  await loadBlock(block);
-};
+function createAccordionBlock(question, answer) {
+  const uuid = generateUUID();
+  const divEl = dl();
+  const btn = dt(
+    { class: 'button peer py-4', 'aria-expanded': false, 'aria-controls': `${uuid}` },
+    button(
+      { type: 'button', class: 'flex w-full items-start justify-between text-left text-gray-900' },
+      h3({ class: 'text-base font-semibold leading-7 my-0' }, question),
+      span({ class: 'ml-6 flex items-center pr-2 my-auto' }),
+    ),
+  );
+  btn.querySelector('span').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="ml-2 h-5 w-5 transition"><path fill-rule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z"></path></svg>';
 
-const buildSubAccordions = async (parentPanel) => {
-  const blockTable = [];
-  let row;
-  [...parentPanel.children].forEach((child) => {
-    if (child.nodeName === 'H3') {
-      if (row) {
-        blockTable.push([{ elems: row }]);
-      }
-      row = [];
-    }
+  const panel = dd(
+    { id: `${uuid}`, class: 'panel pr-12 pb-4 peer-[.show]:block hidden' },
+    div({ class: 'accordion-answer text-base leading-7 text-gray-600 href-text' }),
+  );
 
-    if (row) {
-      row.push(child);
-    }
+  answer.forEach((element) => {
+    panel.querySelector('.accordion-answer').innerHTML += element;
   });
-  // add last row
-  if (row) {
-    blockTable.push([{ elems: row }]);
-    await buildSubAccordion(parentPanel, blockTable);
-  }
-};
 
-let accordionIndex = 0;
-export default async function decorate(block) {
-  block.dataset.accordionIndex = accordionIndex;
-  accordionIndex += 1;
-  const rows = [...block.children];
-  for (let i = 0; i < rows.length; i += 1) {
-    const row = rows[i];
-    row.classList.add('accordion-item');
-    const panel = row.children[0];
-    const buttonSelector = block.classList.contains('sub-accordion') ? 'h3' : 'h2';
-    const header = panel.querySelector(buttonSelector);
-    row.prepend(header);
-    const headerText = header.textContent;
-    header.innerHTML = '';
-    const button = document.createElement('button', 'accordion-trigger', {
-      'aria-expanded': 'false',
-      'aria-controls': `accordion-panel-${block.dataset.accordionIndex}-${i}`,
-      id: `accordion-${block.dataset.accordionIndex}-${i}`,
-    }, createElement('span', 'accordion-title', {}, headerText));
-    header.append(button);
-    panel.classList.add('accordion-panel');
-    panel.setAttribute('id', `accordion-panel-${block.dataset.accordionIndex}-${i}`);
-    panel.setAttribute('role', 'region');
-    panel.setAttribute('aria-labelledby', `accordion-${block.dataset.accordionIndex}-${i}`);
-    panel.setAttribute('hidden', '');
-    // auto open first panel
-    if (i === 0) toggle(row);
+  btn.addEventListener('click', () => toggleAccordion(btn));
+  divEl.append(btn, panel);
+  return divEl;
+}
 
-    button.addEventListener('click', () => {
-      toggle(row);
-    });
+export default function decorate(block) {
+  const questions = [...block.children].map((element) => {
+    const questionElement = element.querySelector(':scope > div > h3');
+    const answerElements = Array.from(element.querySelector(':scope > div').children).slice(1);
+    return {
+      question: questionElement?.textContent,
+      answer: answerElements.map((elem) => elem.outerHTML),
+    };
+  });
 
-    // build sub-accordions from panel
-    if (panel.querySelector(':scope > h3')) {
-      // eslint-disable-next-line no-await-in-loop
-      await buildSubAccordions(panel);
-    }
-  }
+  const filteredQuestions = questions.filter((item) => item.question !== undefined);
+  const accordionItems = filteredQuestions
+    .map((question, index) => createAccordionBlock(question.question, question.answer, index));
+  const accordion = div(
+    { class: 'divide-y divide-gray-900/10' },
+    ...accordionItems,
+  );
+
+  const title = [...block.children][0].querySelector(':scope > div > h2');
+  block.innerHTML = '';
+  if (title && title.textContent) block.append(title, document.createElement('hr'));
+  block.append(accordion);
 }
